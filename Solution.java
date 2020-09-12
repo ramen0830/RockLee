89abde7c12618e295f3498dca34dbd41d4e319c5
ramen0830 <70486512+ramen0830@users.noreply.github.com>
2020-09-11 23:02:18 -0400

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -7,6 +7,41 @@ class Point {
 
 class Solution {    
     /** 09-05-20 **/
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> dict = new HashSet<>(wordList);
+        if (!dict.contains(endWord)) {
+            return 0;
+        }
+        Queue<String> queue = new ArrayDeque<>();
+        queue.offer(beginWord);
+        
+        int level = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                String word = queue.poll();
+                if (Objects.equals(word, endWord)) {
+                    return level;
+                }
+                int len = word.length();
+                for (int j = 0; j < len; j++) {
+                    char[] array = word.toCharArray();
+                    for (int k = 0; k < 26; k++) {
+                        array[j] = (char)('a' + k);
+                        String newWord = new String(array);
+                        if (dict.contains(newWord)) {
+                            queue.offer(newWord);
+                            dict.remove(newWord);
+                        }
+                    }
+                }
+            }
+            level++;
+        }
+        
+        return 0;
+    }
+    
     public void solve(char[][] board) {
         if (board.length == 0 || board[0].length == 0) return;
         int m = board.length, n = board[0].length;

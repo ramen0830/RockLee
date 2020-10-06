9e9b4726c53a5eaeb84285d89e6a7afab31dfc2b
ramen0830 <70486512+ramen0830@users.noreply.github.com>
2020-10-05 22:30:10 -0400

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -5,7 +5,37 @@ class Point {
     }
 }
 
-class Solution {    
+class Solution {
+    /** 10-03-20 **/
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+        return helper(root).target;
+    }
+    
+    private Result helper(TreeNode root) {
+        if (root == null) {
+            return new Result(null, 0);
+        }
+        
+        Result left = helper(root.left);
+        Result right = helper(root.right);
+        
+        if (left.depth == right.depth) {
+            return new Result(root, left.depth + 1);
+        } else if (left.depth < right.depth) {
+            return new Result(right.target, right.depth + 1);
+        } else {
+            return new Result(left.target, left.depth + 1);
+        }        
+    }
+    
+    class Result {
+        TreeNode target;
+        int depth;
+        Result(TreeNode target, int depth) {
+            this.target = target;
+            this.depth = depth;
+        }
+    }
     /** 09-26-20 **/
 
 

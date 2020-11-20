efb5f6f20aad345ca3ef39d64216979a97d7710e
ramen0830 <70486512+ramen0830@users.noreply.github.com>
2020-11-19 22:10:57 -0500

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -12,7 +12,26 @@ class Solution {
 
     
     // 209. Minimum Size Subarray Sum
-
+    // Given an array of n positive integers and a positive integer s
+    // find the minimal length of a contiguous subarray of which the sum ≥ s
+    // If there isn't one, return 0 instead.    
+    public int minSubArrayLen(int s, int[] nums) {
+        int left = 0, right = 0, sum = 0;
+        int ans = Integer.MAX_VALUE;
+        while (right < nums.length) {
+            // [left, right)
+            sum += nums[right];
+            // [left, right]
+            while (sum >= s) {
+                ans = Math.min(ans, right - left + 1);
+                sum -= nums[left];
+                left++;
+            }
+            right++;            
+        }
+        if (ans == Integer.MAX_VALUE) return 0;
+        return ans;
+    }
     
     // 139. Word Break
     HashMap<Integer, Boolean> memo = new HashMap<>();    
